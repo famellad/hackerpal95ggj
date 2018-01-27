cc91126a722d012e24f48c7ebd9b289dc2703d53
Nicholas <N9199@users.noreply.github.com>
2018-01-27 17:09:32 -0300

    Waiting for window textures

--- Hackerpal95/data/game/hackerexe/desafios/lights_out/Light_Panel.gd
+++ Hackerpal95/data/game/hackerexe/desafios/lights_out/Light_Panel.gd
@@ -1,14 +1,13 @@
-extends Panel
+extends Sprite
 
-var size = 9
-var pixel = 90
+var size = 6
+var pixel = 80
 var move_x = 0
-var move_y = 22
+var move_y = 42
 var win = false
 var moves = []
-var map = {3:"un celular", 5:"un computador", 7:"un servidor", 9:"la NASA"}
+var map = {3:"a cellphone", 4:"a PC", 6:"a server", 8:"BASA"}
 var difficulty = "easy"
-var style = StyleBoxFlat.new()
 
 var hackerexe
 
@@ -29,7 +28,6 @@ func leave():
 	popup.margin_left = pixel
 	popup.margin_right = pixel + 400
 	popup.visible = true
-	popup.add_stylebox_override("popup", style)
 	add_child(popup)
 	var text = Label.new()
 	popup.add_child(text)
@@ -37,28 +35,32 @@ func leave():
 	text.margin_left = 50
 	text.align = 1
 	if win:
-		text.text = "Has hackeado " + map[size]
-		hackerexe.last_hacked()
+		text.text = "You've hacked " + map[size]
+		#hackerexe.last_hacked()
 	else:
-		text.text = "Te han atrapado"
+		text.text = "You've been caught"
 
 func set_diff(diff):
 	difficulty = diff
 	if diff == "easy":
 		size = 3
 	elif diff == "medium":
-		size = 5
+		size = 4
 	elif diff == "hard":
-		size = 7
+		size = 6
 	elif diff == "insane":
-		size = 9
+		size = 8
 	create_lights()
 	light_up()
 
 func create_lights():
-	var panel = get_node(".")
-	panel.margin_right = size*pixel + move_x
-	panel.margin_bottom = size*pixel + move_y
+	if size == 3:
+		pixel = 90
+	else:
+		pixel = 80
+	var sprite = get_node(".")
+	#sprite.margin_right = size*pixel + move_x
+	#sprite.margin_bottom = size*pixel + move_y
 	
 	# var barra = get_node("BarraCarga")
 	# barra.scale.x = size*pixel/240.0
@@ -79,19 +81,28 @@ func create_lights():
 
 func light_up():
 	randomize()
-	for i in range(size*2):
-		light(randi()%int(pow(size, 2)) + 1, true)
+	var n = -1
+	var i = 0
+	var l = range(1, pow(size, 2)+1)
+	while i < size:
+		# print(l)
+		n = randi()%len(l)
+		if moves.find(l[n]) == -1:
+			light(l[n], true)
+			i += 1
+			l.erase(l[n])
 
 func light(n, action):
 	var barra = get_node("BarraCarga")
 	var button = get_node("Button"+str(n))
-	#print(n)
-	if n in moves:
-		moves.remove(n)
+	# print(n, " ", moves.find(n))
+	
+	if moves.find(n) != -1:
+		moves.erase(n)
 	else:
 		moves.append(n)
 	moves.sort()
-	# print(moves)
+	print(moves)
 	if action:
 		button.pressed = not button.pressed
 	if n%size != 0:
@@ -137,8 +148,6 @@ func light(n, action):
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	add_stylebox_override("panel", style)
-	style.set_bg_color(Color(0.6, 0.6, 0.6))
 	create_lights()
 	
 	light_up()
