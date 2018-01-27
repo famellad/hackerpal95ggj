e7d31aebbf27ad4d20cddde963907519c6dfcfb0
Felipe Mellado <famellad@gmail.com>
2018-01-27 14:44:19 -0300

    Servidor animado, mejor menú, color arreglado
    Adicionalmente funciones de infectar implementadas en los nodos

--- Hackerpal95/data/game/hackerexe/PC.gd
+++ Hackerpal95/data/game/hackerexe/PC.gd
@@ -3,12 +3,12 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+
 var infected = false
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	#infect()
 	pass
 
 func infect():
--- Hackerpal95/data/game/hackerexe/Servidor.gd
+++ Hackerpal95/data/game/hackerexe/Servidor.gd
@@ -3,19 +3,30 @@ extends Node2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+
+var infect_c = 2
+var infect_t = 0.5
+
 var infected = false
 
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	#infect()
+	infect()
 	pass
 
 func infect():
 	infected = true
 	$Sprite.frame = 1
+	set_process(true)
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _process(delta):
+	infect_t -= delta
+	if infect_t < 0:
+		infect_t = 0.5
+		$Sprite.frame = infect_c
+		infect_c += 1
+		
+		if infect_c == 5:
+			set_process(false)
+	
