ee988526294dbd97c21e41760dc3016c5f22cfd7
Felipe Mellado <famellad@gmail.com>
2018-01-27 09:35:30 -0300

    Barra de carga funcional

---
+++ Hackerpal95/data/game/barra_carga/BarraCarga.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+var level
+var sprite_texture = preload("res://data/game/barra_carga/rectangulo.png")
+
+func _ready():
+	var s
+	for i in range(0, 20):
+		s = Sprite.new()
+		s.hide()
+		s.set_name("box_" + str(i))
+		s.translate(Vector2(13 * i, 0))
+		s.set_texture(sprite_texture)
+		add_child(s)
+	set_level(0.5)
+
+func set_level(new_level):
+	level = new_level
+	new_level = ceil(20 * new_level)
+	
+	for i in range(0, new_level):
+		get_node("box_" + str(i)).show()
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
