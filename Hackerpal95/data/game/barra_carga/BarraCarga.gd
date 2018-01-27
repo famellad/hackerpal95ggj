4032f20704704cda2074eb73910b734a11b75cd6
Felipe Mellado <famellad@gmail.com>
2018-01-27 09:44:19 -0300

    Primera implementación de HackerHP

--- Hackerpal95/data/game/barra_carga/BarraCarga.gd
+++ Hackerpal95/data/game/barra_carga/BarraCarga.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var level
+var level = 0
 var sprite_texture = preload("res://data/game/barra_carga/rectangulo.png")
 
 func _ready():
@@ -11,10 +11,13 @@ func _ready():
 		s.set_name("box_" + str(i))
 		s.translate(Vector2(13 * i, 0))
 		s.set_texture(sprite_texture)
+		s.centered = false
 		add_child(s)
-	set_level(0.5)
 
 func set_level(new_level):
+	if new_level > 1:
+		new_level = 1
+	
 	level = new_level
 	new_level = ceil(20 * new_level)
 	
@@ -22,6 +25,6 @@ func set_level(new_level):
 		get_node("box_" + str(i)).show()
 
 #func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+	# Called every frame. Delta is time since last frame.
+	# Update game logic here.
+#	set_level(level + delta)
