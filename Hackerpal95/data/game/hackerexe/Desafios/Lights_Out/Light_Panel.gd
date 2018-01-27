d1eefc249641a93b6e9c583c5d68eb0fbd355b3e
Nicholas <N9199@users.noreply.github.com>
2018-01-27 02:53:37 -0300

    Lights are almost finished*

--- Hackerpal95/data/game/hackerexe/Desafios/Lights_Out/Light_Panel.gd
+++ Hackerpal95/data/game/hackerexe/Desafios/Lights_Out/Light_Panel.gd
@@ -4,12 +4,24 @@ var size = 3
 var pixel = 100
 var move_x = 0
 var move_y = 0
+var win =  false
 
 func print_grid(grid):
 	var temp = ""
 	for line in grid:
 		temp += str(line)+"\n"
 	print(temp)
+
+func set_diff(diff):
+	if diff == "easy":
+		size = 3
+	elif diff == "medium":
+		size = 5
+	elif diff == "hard":
+		size = 7
+	elif diff == "insane":
+		size = 9
+	create_lights()
 
 func create_lights():
 	var panel = get_node(".")
@@ -24,6 +36,7 @@ func create_lights():
 			button.margin_top = pixel*j + 2 + move_y
 			button.margin_bottom = pixel*(j+1) - 2 + move_y
 			button.toggle_mode = true
+			button.pressed = true
 			add_child(button)
 
 func light_up():
@@ -53,14 +66,21 @@ func light(n, action):
 		button.pressed = not button.pressed
 	
 	var grid = []
+	var temp = true
 	for i in range(1, size+1):
 		var line = []
 		for j in range(size):
 			button = get_node("Button"+str(i+size*j))
 			line.append(button.pressed)
+			if not button.pressed:
+				temp = false
 		grid.append(line)
 	print_grid(grid)
-		
+
+	if temp:
+		win = true
+	
+	print("Win: ", win)
 
 func _ready():
 	# Called every time the node is added to the scene.
